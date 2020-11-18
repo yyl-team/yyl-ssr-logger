001d16b412046465c48a040c036cc0acbee6c803
liudaojie <liudaojie@yy.com>
2020-11-18 10:59:40 +0800

    feat: 修改 content 输出到日志格式改为 string

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import json from 'rollup-plugin-json'
-import { terser } from 'rollup-plugin-terser'
 
 function buildBanner(type) {
   return [
@@ -43,14 +42,7 @@ export default [
         sourcemap: false
       }
     ],
-    plugins: config.plugins.concat([
-      IS_PUBLISH &&
-        terser({
-          compress: {
-            passes: 2
-          }
-        })
-    ]),
+    plugins: config.plugins,
     external: config.external
   },
   {
